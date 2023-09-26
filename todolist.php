d91c749920ba91650b897943634e276c18e87add
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2023-09-26 09:50:00 +0200

    ADD- CSS & Js dynamic

--- todolist.php
+++ todolist.php
@@ -1,16 +1,18 @@
-<h1>Todo List</h1>
 
-<form method="post" id="formTask">
-  <input type="text" name="task" placeholder="Tâche" required />
-  <input type="submit" value="Ajouter" />
-</form>
+<div class="container">
+  <div class="todoapp">
+    <h2>To-Do List<img src="images/todoLogo.png" alt="logo"></h2>
+    <div class="row">
+      <input type="text" placeholder="Ajouter une tâche" id="input-box">
+      <button id="add-button" onclick="addTask()">Ajouter</button>
+    </div>
+    <ul id="list-container">
+      <!-- <li class="checked">Tâche N°1</li>
+        <li>Tâche N°2</li>
+        <li>Tâche N°3</li> -->
+    </ul>
+  </div>
 
-<div class="displayTodos">
-  <ul id="todos">
-    
-   <!-- foreach ($todos as $todo) {
-      echo "<li id='" . $todo->id . "'><span>" . $todo->task . "</span><button class='delete'>X</button></li>";
-    } -->
-  </ul>
-  
 </div>
+
+<script src="Js/todoFunction.js"></script>
